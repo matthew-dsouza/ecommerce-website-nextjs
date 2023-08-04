055d91e58d8d0dd15ccb58659703ab374ddb6e84
Matthew Dsouza <matthewdsouza.one@gmail.com>
2023-08-04 16:11:36 +0530

    updated title

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -8,7 +8,7 @@ const Layout = ({ children }) => {
   return (
     <div className="layout">
       <Head>
-        <title>WaveKnox Electronics</title>
+        <title>EcommercePro Electronics</title>
       </Head>
       <header>
         <Navbar />
